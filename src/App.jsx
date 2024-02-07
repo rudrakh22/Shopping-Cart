371eefef22d9ef814c497029dbeeeec13bba549b
rudraksh22 <rudrakshg1914@gmail.com>
2024-02-07 21:25:40 +0530

    Adding Final Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { Route,Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/Navbar"
+import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
 
 function App() {
   return (
-    <main className="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 className="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p className="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" className="mx-auto" />
-    </main>
+    <div >
+      <div className="bg-slate-900">
+        <Navbar/>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/cart" element={<Cart/>} />
+      </Routes>
+    </div>
   );
 }
 
---
+++ src/Components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="w-full h-[100vh] flex justify-center items-center">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Spinner;
